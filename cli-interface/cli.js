491dc55493494b8a473aac1dd98fd430a22d4ae7
Marcus Vinicius Rodrigues Andrade <vinnyaju@gmail.com>
2024-12-29 14:39:29 -0300

    Adicionando consultas na cli e alterando o comando de criação de chaves.

--- cli-interface/cli.js
+++ cli-interface/cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { createKey } from './commands/crypto-keys.js';
+import { createKey } from './commands/createKey.js';
+import { getKeysByClientId, getKeysByIdAndUsage, getKeysByUsage } from '../database/operations.js'; // Substitua pelo caminho correto
+
 import dotenv from 'dotenv';
 
 const program = new Command();
@@ -14,8 +16,51 @@ program
 
 // Comando para gerenciar usuários
 program
-  .command('crypto-keys')
+  .command('createKey')
   .description('Gerenciar Chaves Criptográficas.')
   .action(createKey);
+
+
+// Comando: Consulta por `clientId`
+program
+  .command('get-by-client-id <clientId>')
+  .description('Consulta chaves por clientId')
+  .action((clientId) => {
+    const keys = getKeysByClientId(clientId);
+    if (keys.length > 0) {
+      console.log(`Chaves encontradas para clientId "${clientId}":`);
+      console.log(keys);
+    } else {
+      console.log(`Nenhuma chave encontrada para clientId "${clientId}".`);
+    }
+  });
+
+// Comando: Consulta por `id` e `usage`
+program
+  .command('get-by-id-and-usage <id> <usage>')
+  .description('Consulta chaves por ID e uso')
+  .action((id, usage) => {
+    const keys = getKeysByIdAndUsage(id, usage);
+    if (keys.length > 0) {
+      console.log(`Chaves encontradas para ID "${id}" com uso "${usage}":`);
+      console.log(keys);
+    } else {
+      console.log(`Nenhuma chave encontrada para ID "${id}" com uso "${usage}".`);
+    }
+  });
+
+// Comando: Consulta por `usage`
+program
+  .command('get-by-usage <usage>')
+  .description('Consulta chaves por uso')
+  .action((usage) => {
+    const keys = getKeysByUsage(usage);
+    if (keys.length > 0) {
+      console.log(`Chaves encontradas com uso "${usage}":`);
+      console.log(keys);
+    } else {
+      console.log(`Nenhuma chave encontrada com uso "${usage}".`);
+    }
+  });
 
 program.parse(process.argv);
